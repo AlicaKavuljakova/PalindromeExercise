0ab9b1b237ca224b2c04b8a1684a0bf0338a1fdd
alica kavuljakova <drobeek@gmail.com>
2023-05-22 18:31:04 -0700

    hw

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+  public class WordSmith
+    {
+        public bool isPalindrome(string word)
+        {
+            if(word==null || word.Length==0) return false;
+          var orig= word.ToLower().ToCharArray();
+            var reversed=word.ToCharArray().Reverse();
+          return orig.SequenceEqual(reversed);
+        }
+    }
+}
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmithTest.cs
@@ -0,0 +1,32 @@
+﻿using PalindromeExercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmithTest
+    {
+        [Theory]
+        [InlineData("potato", false)]
+        [InlineData("", false)]
+
+        [InlineData("Kayak", true)]
+
+        [InlineData("level", true)]
+
+        [InlineData("bob", true)]
+
+        [InlineData("deed", true)]
+        [InlineData(null,false)]
+        void isPalindromeTest(string word, bool expected)
+        {
+            var test = new WordSmith();
+            var actual = test.isPalindrome(word);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
